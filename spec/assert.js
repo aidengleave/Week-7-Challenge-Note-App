ed234da644a22298d6b0fcd7b784d9b3cffbfae6
Aiden Gleave <aidengleave@gmail.com>
2019-02-20 13:58:12 +0000

    Added if statement to log passing test

--- spec/assert.js
+++ spec/assert.js
@@ -1,7 +1,9 @@
 var assert = {
   isTrue: function(assertionToCheck) {
     if (!assertionToCheck) {
-      throw new Error("assertion failed: " + assertionToCheck + " is not truthy");
+      throw new Error("Test failed: " + assertionToCheck + " is not truthy");
+    } else {
+      console.log("Test Passed")
     }
   }
 };
